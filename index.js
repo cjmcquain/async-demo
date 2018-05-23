246c2ffa6400afab3503402184841ee49cc5f598
Charles McQuain <charlesmcquain@gmail.com>
2018-05-22 22:18:27 -0700

    Simulated async code, retrieving a user from database

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 console.log("Before");
-setTimeout(() => {
-  console.log("Reading a user from a database...");
-}, 2000);
+const user = getUser(1);
 console.log("After");
+
+function getUser(id) {
+  setTimeout(() => {
+    console.log("Reading a user from a database...");
+    return { id: id, gitHubUsername: "mosh" };
+  }, 2000);
+}
